b29c223c3a8ddaf51c05e83369b271d507f7fa65
tuntunking <kwakmj159@gmail.com>
2023-06-25 23:45:46 +0900

    초기 커밋.

---
+++ MVVMtest/Models/Person.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMtest.Models
+{
+    public class Person : ObservableObject // toolkit에서 제공, 
+    {
+		private int _id;
+		public int Id
+		{
+			get { return _id; }
+			set
+			{
+				SetProperty(ref _id, value);
+			}
+		}
+
+		private string? _name;
+		public string? Name
+		{
+			get { return _name; }
+			set 
+			{ 
+				SetProperty(ref _name, value);
+			}
+		}
+
+        private bool _gender;
+        public bool Gender
+        {
+            get { return _gender; }
+            set
+            {
+                SetProperty(ref _gender, value);
+            }
+        }
+
+    }
+}
---
+++ MVVMtest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MVVMtest.Models;
+using MVVMtest.ViewModels.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMtest.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private IList<bool>? _genderTypes = new List<bool>() { true, false };
+        public IList<bool>? GenderTypes
+        {
+            get => _genderTypes;
+        }
+
+        private IList<Person>? _people;
+        public IList<Person>? People
+        {
+            get => _people;
+            set => SetProperty(ref _people, value);
+        }
+
+        private Person? _selectedPerson;
+        public Person? SelectedPerson
+        {
+            get => _selectedPerson;
+            set => SetProperty(ref _selectedPerson, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            People = new List<Person>
+            {
+                new Person{Id = 119302, Name = "톰", Gender = true},
+                new Person{Id = 119303, Name = "탐", Gender = false},
+                new Person{Id = 119304, Name = "팀", Gender = true},
+                new Person{Id = 119305, Name = "뚱", Gender = true},
+                new Person{Id = 119306, Name = "캣", Gender = false},
+                new Person{Id = 119307, Name = "땅", Gender = true},
+
+            };
+        }
+    }
+}
